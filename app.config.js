037fad27537e6da68809411b90eddfe0a7f4c7a0
Jarrod Tomholt <jarrodtomholt@gmail.com>
2020-11-19 22:25:52 +1100

    adding nuxt frontend

---
+++ app.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+
+// eslint-disable-next-line import/no-absolute-path
+import original from './resources/app/nuxt.config'
+
+const config = { ...original }
+
+config.rootDir = resolve('./resources/app')
+config.modules = [...(config.modules || []), 'nuxt-laravel']
+config.laravel = {
+    server: false,
+    dotEnvExport: true
+}
+
+config.router = {
+    ...config.router,
+    base: '/app/'
+}
+
+export default config
