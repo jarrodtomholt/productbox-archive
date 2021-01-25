8561a8c29e0cae375f92f23eaa4ba8ff52dfe119
Jarrod Tomholt <jarrodtomholt@gmail.com>
2021-01-25 11:37:01 +1100

    composer update

--- resources/app/tailwind.config.js
+++ resources/app/tailwind.config.js
@@ -1,10 +1,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     purge: [],
     darkMode: false,
     theme: {
         extend: {
+            colors: {
+                gray: colors.trueGray,
+            },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
